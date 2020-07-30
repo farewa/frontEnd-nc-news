e8667405d8f779f556ad449d238ac19bb825185f
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-07-30 12:12:31 +0100

    created AllArticles component that displays all the article data

---
+++ src/components/articles/AllArticles.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { ArticleList } from "./ArticleList";
+import { ArticleWrapper } from "../styled/lib";
+import { useArticles} from '../../hooks/useArticles'
+
+export const AllArticles = () => {
+
+  const { loading, data, error} = useArticles()
+
+  return (
+    <ArticleWrapper>
+      <h1>Articles</h1>
+      {error && <p>error</p>}
+      {loading ? (
+        <p>loading...</p>
+      ) : (
+        <ul>{data.articles && <ArticleList articles={data.articles} />}</ul>
+      )}
+    </ArticleWrapper>
+  );
+};
