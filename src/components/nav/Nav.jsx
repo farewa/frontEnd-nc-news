2f4f9a5dfb295eb591bbb53a15de90dc5b054f5e
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-07-30 12:18:19 +0100

    created general nav component

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {LeftNav} from './LeftNav'
+import { StyledNav } from "../styled/lib";
+
+export const Nav = () => {
+  return (
+    <StyledNav>
+      <LeftNav/>
+    </StyledNav>
+  );
+};
