717b024a3ce111dae120e95e8d14379df7802841
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-07-30 11:55:07 +0100

    created App function that renders initial core components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Router } from "@reach/router";
+import { Home } from "./components/home/Home";
+import { AllArticles } from "./components/articles/AllArticles";
+import {SingleArticle} from "./components/articles/SingleArticle"
+import { AppWrapper } from "./components/styled/lib";
+import { Nav } from "./components/nav/Nav";
+import { ArticleByTopic } from "./components/topics/ArticleByTopic";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <Nav/>
+      <Router>
+        <Home path="/" />
+        <AllArticles path="/articles" />
+        <ArticleByTopic path = "/:topic"/>
+        <SingleArticle path="/articles/:article_id" />
+      </Router>
+    </AppWrapper>
   );
-}
+};
 
 export default App;
