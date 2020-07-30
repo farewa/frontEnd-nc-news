dee9a84e7f26c4b6aa820adb2b45fcff356a2b19
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-07-30 12:00:38 +0100

    created nav component with topics in nav bar

---
+++ src/components/nav/LeftNav.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useCallback, useEffect } from "react";
+import { Link } from "@reach/router";
+import { UL } from "../styled/lib";
+import axios from "axios";
+
+export const LeftNav = () => {
+  const [topics, setTopics] = useState([]);
+  const [error, setError] = useState({});
+
+  const fetchTopics = useCallback(() => {
+    const fetchingTopics = async () => {
+      try {
+        const response = await axios.get(
+          "https://b-end-nc-news-app.herokuapp.com/api/topics"
+        );
+        setTopics(response.data.topics);
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchingTopics();
+  }, []);
+
+  useEffect(() => {
+    let mounted = true;
+
+    if (mounted) {
+      fetchTopics();
+    }
+
+    return () => {
+      mounted = false;
+    };
+  }, [fetchTopics]);
+
+  return (
+    <UL>
+      <li>
+        {" "}
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        {" "}
+        <Link to="/articles">Articles</Link>
+      </li>
+      {topics &&
+        topics.map((topic) => {
+          return (
+            <li>
+              {" "}
+              <Link to={`/${topic.slug}`}> {topic.slug} </Link>
+            </li>
+          );
+        })}
+    </UL>
+  );
+};
