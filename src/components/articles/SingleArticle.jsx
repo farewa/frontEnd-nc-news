9db88263a3b61573c5e83a70d08721cd06f69ff2
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-08-01 15:59:46 +0100

    displaying single article information

--- src/components/articles/SingleArticle.jsx
+++ src/components/articles/SingleArticle.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import moment from "moment";
+import { useSingleArticle } from "../../hooks/useSingleArticle";
+import { ArticleWrapper } from "../styled/lib";
 
-export const SingleArticle = () => {
-  return <p>single article</p>;
+export const SingleArticle = ({ article_id }) => {
+  const { loading, data, error } = useSingleArticle(article_id);
+
+  console.log("in single article");
+
+  return (
+    <>
+      {error && <p>error</p>}
+      {loading ? (
+        <p>loading...</p>
+      ) : (
+        data.article && (
+          <ArticleWrapper>
+            <h1>{data.article.title}</h1>
+            <h3>author: {data.article.author}</h3>
+            <h3>created {moment(data.article.created_at).fromNow()}</h3>
+            <p>{data.article.body}</p>
+            <p>Comment Count: {data.article.comment_count}</p>
+          </ArticleWrapper>
+        )
+      )}
+    </>
+  );
 };
