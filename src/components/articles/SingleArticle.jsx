6ae41c4c5644fed6891cca952a332a9db2f214db
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-08-01 19:41:06 +0100

    added button that would show associated comments when clicked

--- src/components/articles/SingleArticle.jsx
+++ src/components/articles/SingleArticle.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 import { useSingleArticle } from "../../hooks/useSingleArticle";
 import { ArticleWrapper } from "../styled/lib";
+import { SingleArticleComments } from "../comments/SingleArticleComments";
 
 export const SingleArticle = ({ article_id }) => {
-  const { loading, data, error } = useSingleArticle(article_id);
+  const type = "singleArticle";
+  const { loading, data, error } = useSingleArticle(article_id, type);
+
+  const [showComment, setComment] = useState(false);
 
   console.log("in single article");
 
@@ -16,11 +20,19 @@ export const SingleArticle = ({ article_id }) => {
       ) : (
         data.article && (
           <ArticleWrapper>
-            <h1>{data.article.title}</h1>
+            <h2>{data.article.title}</h2>
             <h3>author: {data.article.author}</h3>
             <h3>created {moment(data.article.created_at).fromNow()}</h3>
             <p>{data.article.body}</p>
             <p>Comment Count: {data.article.comment_count}</p>
+            <button
+              onClick={() => {
+                setComment(!showComment);
+              }}
+            >
+              comments
+            </button>
+            {showComment && <SingleArticleComments article_id={article_id} />}
           </ArticleWrapper>
         )
       )}
