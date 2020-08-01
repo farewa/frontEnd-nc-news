533d2972e12db2abbfdcf607b3dd136b53993236
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-08-01 20:39:33 +0100

    created comment component that shows all comments

--- src/components/comments/SingleArticleComments.jsx
+++ src/components/comments/SingleArticleComments.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import { useSingleArticle } from "../../hooks/useSingleArticle";
+import { CommentDiv } from "../styled/lib";
+import moment from "moment";
 
 export const SingleArticleComments = ({ article_id }) => {
   const type = "comments";
 
   const { loading, data, error } = useSingleArticle(article_id, type);
 
-  console.log(data);
+  console.log("in comments");
 
-  return <p> comments</p>;
+  return (
+    <section>
+      <article>
+        {error && <p>error</p>}
+        {loading ? (
+          <p>loading...</p>
+        ) : data.comments ? (
+          data.comments.map((comment) => {
+            return (
+              <CommentDiv key={comment.comment_id}>
+                <p>{comment.body}</p>
+                <p>created {moment(comment.created_at).fromNow()}</p>
+              </CommentDiv>
+            );
+          })
+        ) : (
+          <p>no comments yet</p>
+        )}
+      </article>
+    </section>
+  );
 };
