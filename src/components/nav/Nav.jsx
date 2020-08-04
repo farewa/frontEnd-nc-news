07fc3c5562f224432592c8cd8b07c2f8932b0594
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-08-04 21:56:16 +0100

    added loginbox component to Nac

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import {LeftNav} from './LeftNav'
+import { LeftNav } from "./LeftNav";
+import { LogInBox } from "../login/Login";
 import { StyledNav } from "../styled/lib";
 
 export const Nav = () => {
   return (
     <StyledNav>
-      <LeftNav/>
+      <LeftNav />
+      <LogInBox />
     </StyledNav>
   );
 };
