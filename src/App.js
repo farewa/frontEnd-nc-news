99c26df03d4de4c4d71e5044ba6bea9fd5b03ede
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-08-04 21:52:53 +0100

    creating contexts to pass down username value to other components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useState, createContext, useCallback, useMemo } from "react";
 import "./App.css";
 import { Router } from "@reach/router";
 import { Home } from "./components/home/Home";
 import { AllArticles } from "./components/articles/AllArticles";
-import {SingleArticle} from "./components/articles/SingleArticle"
+import { SingleArticle } from "./components/articles/SingleArticle";
 import { AppWrapper } from "./components/styled/lib";
 import { Nav } from "./components/nav/Nav";
 import { ArticleByTopic } from "./components/topics/ArticleByTopic";
 
+export const UserContext = createContext();
+export const SetUserContext = createContext();
+
 const App = () => {
+  const [userName, setUsername] = useState("");
+
+  const settingUsername = useCallback(() => setUsername(userName), [userName]);
+
+  const getUsername = useMemo(() => ({settingUsername}), [settingUsername])
+
   return (
     <AppWrapper>
-      <Nav/>
-      <Router>
-        <Home path="/" />
-        <AllArticles path="/articles" />
-        <ArticleByTopic path = "/:topic"/>
-        <SingleArticle path="/articles/:article_id" />
-      </Router>
+      <UserContext.Provider value={userName}>
+        <SetUserContext.Provider value={getUsername()}>
+          <Nav />
+          <Router>
+            <Home path="/" />
+            <AllArticles path="/articles" />
+            <ArticleByTopic path="/:topic" />
+            <SingleArticle path="/articles/:article_id" />
+          </Router>
+        </SetUserContext.Provider>
+      </UserContext.Provider>
     </AppWrapper>
   );
 };
