7be222988fba484b17300067813f8ae015d691e8
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-08-04 20:35:57 +0100

    added topics and users api request

--- src/api.js
+++ src/api.js
@@ -4,3 +4,13 @@ export const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
 
+export const getTopics = async () => {
+  const allTopics = await axiosInstance.get("/topics");
+  return allTopics.data.topics;
+};
+
+export const getUser = async (username) => {
+  const singleUser = await axiosInstance.get(`/users/${username}`);
+
+  return singleUser.data.user;
+};
