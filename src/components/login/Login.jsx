824a4ccdf8e7221d96c2680ec84b4d764946441f
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-08-05 08:48:16 +0100

    completed logout functionality

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -31,44 +31,44 @@ export const LogInBox = () => {
         handleReset,
         isSubmitting,
       }) => (
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="username"> Username</label>
-              <select
-                name="username"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.username}
-              >
-                <option value="" label="select a username"></option>
-                <option value="tickle122" label="tickle122"></option>
-                <option value="grumpy19" label="grumpy19"></option>
-                <option value="happyamy2016" label="happyamy2016"></option>
-                <option value="cooljmessy" label="cooljmessy"></option>
-                <option value="weegembump" label="weegembump"></option>
-                <option value="jessjelly" label="jessjelly"></option>
-              </select>
-              {errors.username && touched.username && (
-                <div className="input-feedback">{errors.username}</div>
-              )}
-            </div>
-            <button type="submit" disabled={isSubmitting}>
-              login
-          </button>
-            {user && (
-              <div>
-                <p>logged in as {user}</p>
-                <button
-                  onClick={handleReset}
-                  disabled={isSubmitting}
-                  type={"reset"}
-                >
-                  reset
-              </button>
-              </div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username"> Username</label>
+            <select
+              name="username"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.username}
+            >
+              <option value="" label="select a username"></option>
+              <option value="tickle122" label="tickle122"></option>
+              <option value="grumpy19" label="grumpy19"></option>
+              <option value="happyamy2016" label="happyamy2016"></option>
+              <option value="cooljmessy" label="cooljmessy"></option>
+              <option value="weegembump" label="weegembump"></option>
+              <option value="jessjelly" label="jessjelly"></option>
+            </select>
+            {errors.username && touched.username && (
+              <div className="input-feedback">{errors.username}</div>
             )}
-          </form>
-        )}
+          </div>
+          <button type="submit" disabled={isSubmitting}>
+            login
+          </button>
+          {user && (
+            <div>
+              <p>logged in as {user}</p>
+              <button
+                onClick={() => setUser("")}
+                disabled={isSubmitting}
+                type={"reset"}
+              >
+                logout
+              </button>
+            </div>
+          )}
+        </form>
+      )}
     </Formik>
   );
 };
