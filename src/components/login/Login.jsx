174f437ceb4e1158fdf6a8ca67a02594fd323456
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-08-19 09:40:07 +0100

    added key to map of users and removed raw css from file

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -4,9 +4,7 @@ import { Formik } from "formik";
 import { StyledButton, MessageTag } from "../styled/lib";
 import * as Yup from "yup";
 import * as api from "../../api";
-import {LogOut} from './Logout'
-
-
+import { LogOut } from "./Logout";
 
 const validationSchema = Yup.object().shape({
   username: Yup.string().required("choose a username"),
@@ -44,61 +42,65 @@ export const LogInBox = () => {
 
   return (
     <div>
-    {user ? <LogOut /> : 
-    <Formik
-      initialValues={{ user }}
-      validationSchema={validationSchema}
-      onSubmit={async (values, { setSubmitting }) => {
-        setSubmitting(true);
-        const usernameResponse = await api.getUser(values.username);
-        setUser(usernameResponse.username);
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        handleReset,
-        isSubmitting,
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="username"> username</label>
-            <select
-              name="username"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.username}
-            >
-            <option value="" label="select a username"></option>
-            {users.map((user) => {
-              return <option value ={user.username} label={user.username}></option>
-            })}
-            </select>
-            {errors.username && touched.username && (
-              <div className="input-feedback">{errors.username}</div>
-            )}
-            <StyledButton type="submit" disabled={isSubmitting}>
-              login
-            </StyledButton>
-            {user && (
-              <div>
-                <MessageTag>logged in as {user}</MessageTag>
-                <StyledButton
-                  onClick={() => setUser("")}
-                  disabled={isSubmitting}
-                  type={"reset"}
-                >
-                  logout
-                </StyledButton>
-              </div>
-            )}
-        </form>
+      {user ? (
+        <LogOut />
+      ) : (
+        <Formik
+          initialValues={{ user }}
+          validationSchema={validationSchema}
+          onSubmit={async (values, { setSubmitting }) => {
+            setSubmitting(true);
+            const usernameResponse = await api.getUser(values.username);
+            setUser(usernameResponse.username);
+          }}
+        >
+          {({
+            values,
+            errors,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            handleReset,
+            isSubmitting,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="username"> username</label>
+              <select
+                name="username"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.username}
+              >
+                <option value="" label="select a username"></option>
+                {users.map((user) => {
+                  return (
+                    <option
+                      value={user.username}
+                      label={user.username}
+                      key={user.username}
+                    ></option>
+                  );
+                })}
+              </select>
+              <StyledButton type="submit" disabled={isSubmitting}>
+                login
+              </StyledButton>
+              {user && (
+                <div>
+                  <MessageTag>logged in as {user}</MessageTag>
+                  <StyledButton
+                    onClick={() => setUser("")}
+                    disabled={isSubmitting}
+                    type={"reset"}
+                  >
+                    logout
+                  </StyledButton>
+                </div>
+              )}
+            </form>
+          )}
+        </Formik>
       )}
-    </Formik>
-    }
     </div>
   );
 };
