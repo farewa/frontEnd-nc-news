55a006002f0cd8a0a686fd77bdb9595434b1633b
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-08-19 09:45:49 +0100

    created function that will set page number in order to pass the information to the pagination component

--- src/components/articles/AllArticles.jsx
+++ src/components/articles/AllArticles.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { ArticleList } from "./ArticleList";
 import { ArticleWrapper } from "../styled/lib";
 import { useArticles } from "../../hooks/useArticles";
+import { Pagination } from "../Pagination";
 
 export const AllArticles = () => {
-  const { loading, data, error } = useArticles();
+  const [currentPage, setCurrentPage] = useState(1);
+  const { loading, data, error } = useArticles("", currentPage);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   return (
     <main>
@@ -14,7 +20,10 @@ export const AllArticles = () => {
         {loading ? (
           <p>loading...</p>
         ) : (
-          <ul>{data.articles && <ArticleList articles={data.articles} />}</ul>
+          <div>
+            <ul>{data.articles && <ArticleList articles={data.articles} />}</ul>
+            <Pagination totalArticles={data.total_count} paginate={paginate} />
+          </div>
         )}
       </ArticleWrapper>
     </main>
